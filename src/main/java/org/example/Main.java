2e0428c90cc0d6c3ca3f51946c4cbca1c9bdc44c
Juan Oliver <juan_oliver@live.se>
2023-07-19 12:26:31 +0200

    Exercises section one to four

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        double var1 = 20.00;
+        double var2 = 80.00;
+
+        double sum = ((var1 + var2) * 100);
+        System.out.println("\nMy values total = " +sum);
+
+        double mod = sum % 40.00;
+        System.out.println("\nThe remainer is = " +mod);
+
+        boolean zero = (mod == 0) ? true : false;
+
+        if (!zero) {
+            System.out.println("Got some remainer");
+        }
+
+        System.out.println("\nGot no remainer = " +zero);
+
+
+
+
+
+    }
+}
